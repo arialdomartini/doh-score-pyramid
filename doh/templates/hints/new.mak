9f4c126cbcdfdf5e647636c14eeb79eb8e8fe71f
Arialdo Martini <arialdomartini@gmail.com>
2014-02-13 18:51:11 +0100

    Hints templates in a separate directory

---
+++ doh/templates/hints/new.mak
@@ -0,0 +1,5 @@
+<%namespace name="master" file="../master.mak"/>
+<%master:layout>
+    <%def name="title()">Register a new hint</%def>
+    add new
+</%master:layout>
--- doh/views.py
+++ doh/views.py
@@ -14,6 +14,6 @@ def home(request):
     hint = DBSession.query(Hint).first()
     return {'project': 'doh', 'hint': hint.question}
 
-@view_config(route_name='hints.new', renderer='hint_new.mak')
+@view_config(route_name='hints.new', renderer='hints/new.mak')
 def hint_new(request):
     return {}
